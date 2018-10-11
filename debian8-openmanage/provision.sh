72d79a82c317db3adab195db8179508f0dc7f894
Julien Garnier <julien.garnier@braincube.com>
2018-10-11 16:23:23 +0200

    openssl package added

--- debian8-openmanage/provision.sh
+++ debian8-openmanage/provision.sh
@@ -2,4 +2,4 @@ echo 'deb http://linux.dell.com/repo/community/debian jessie openmanage' | sudo 
 sudo gpg --keyserver pool.sks-keyservers.net --recv-key 1285491434D8786F
 gpg -a --export 1285491434D8786F | sudo apt-key add -
 apt-get update
-apt-get install -y srvadmin-all
+apt-get install -y srvadmin-all openssl

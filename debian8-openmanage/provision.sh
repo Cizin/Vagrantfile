df5b7e5509ef5932eb71f09005abddb6f35bb94e
Julien Garnier <julien.garnier@braincube.com>
2018-11-06 09:50:31 +0100

    no need to install libssl.1.0.2

--- debian8-openmanage/provision.sh
+++ debian8-openmanage/provision.sh
@@ -2,5 +2,5 @@ echo 'deb http://linux.dell.com/repo/community/debian jessie openmanage' | sudo 
 sudo gpg --keyserver pool.sks-keyservers.net --recv-key 1285491434D8786F
 gpg -a --export 1285491434D8786F | sudo apt-key add -
 apt-get update
-apt-get install -y srvadmin-all openssl libssl1.0.2
-ln -svf /usr/lib/x86_64-linux-gnu/libssl.so.1.0.2 /opt/dell/srvadmin/lib64/libssl.so
+apt-get install -y srvadmin-all openssl 
+ln -svf /usr/lib/x86_64-linux-gnu/libssl.so.1.0.0 /opt/dell/srvadmin/lib64/libssl.so
